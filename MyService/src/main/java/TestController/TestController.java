787f64c5d3b6ecb403e628e733144cc930883890
ddongbu <sangmin5848@naver.com>
2023-10-31 13:52:53 +0900

    commit

--- MyService/src/main/java/TestController/TestController.java
+++ MyService/src/main/java/TestController/TestController.java
@@ -1,2 +1,15 @@
-package TestController;public class TestController {
+package TestController;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/api/GetMappingTest")
+    public static String GetMappingTest() {
+        return "GetMappingTest";
+    }
+
+
 }
